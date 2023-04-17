2bf1ce60d3f3e5163ff528ac5d6113fd4dc5b3c7
Tanpham433 <128810504+Tanpham433@users.noreply.github.com>
2023-04-17 18:45:04 +0700

    Add files via upload

--- cau_do.php
+++ cau_do.php
@@ -9,22 +9,16 @@
  * License: GPL2
  */
 
+
+
 // Thêm ô nhập liệu vào trang đăng bài mới
 function cau_do_add_meta_box() {
-    add_meta_box(
-        'cau_do_meta_box',
-        'Đáp án câu đố',
-        'cau_do_render_meta_box',
-        'post',
-        'normal',
-        'high'
-    );
+    add_meta_box('cau_do_meta_box','Đáp án câu đố','cau_do_render_meta_box','post');
 }
 add_action( 'add_meta_boxes', 'cau_do_add_meta_box' );
 
 // Hiển thị ô nhập liệu
 function cau_do_render_meta_box( $post ) {
-    wp_nonce_field( 'cau_do_save_meta_box_data', 'cau_do_meta_box_nonce' );
     echo '<label for="cau_do_dap_an">Đáp án:</label>';
     echo '<input type="text" id="cau_do_dap_an" name="cau_do_dap_an" value="' . esc_attr( $value ) . '" />';
 }
@@ -36,28 +30,34 @@ function cau_do_save_meta_box_data( $post_id ) {
 }
 add_action( 'save_post', 'cau_do_save_meta_box_data' );
 
-// Thêm nút "Đáp án" vào trang xem bài viết
-function cau_do_add_button() {
+function add_answer_button( $content ) {
     global $post;
-        $url = get_permalink( $post->ID );
-        echo '<a href="' . $url . '?dap-an=true" class="button">Đáp án</a>';
+    $url = get_permalink( $post->ID );
+    $button_html = '<div><a href="' . $url . '?dap-an=true" class="button">Đáp án</a></div>';
+    $content .= $button_html;
+    return $content;
 }
-add_action( 'the_content', 'cau_do_add_button',2 );
+add_filter( 'the_content', 'add_answer_button' );
 
 // Hiển thị phần đáp án của câu đố
-function cau_do_show_answer() {
+function cau_do_get_answer() {
+    $answer = '';
+
     if ( isset( $_GET['dap-an'] ) && $_GET['dap-an'] == 'true' ) {
         $value = get_post_meta( get_the_ID(), 'cau_do_dap_an', true );
         if ( $value ) {
-            echo '<div class="cau-do-dap-an"><strong>Đáp án:</strong> ' . esc_html( $value ) . '</div>';
+            $answer = '<div class="cau-do-dap-an"><strong>Đáp án:</strong> ' . esc_html( $value ) . '</div>';
         }
     }
+
+    return $answer;
 }
-add_action( 'the_content', 'cau_do_show_answer' );
+
 // Hiển thị nội dung bài post
 function cau_do_show_content( $content ) {
     if ( is_singular( 'post' ) ) {
-        $content .= get_the_content();
+        $answer = cau_do_get_answer();
+        $content .= $answer;
     }
     return $content;
 }
